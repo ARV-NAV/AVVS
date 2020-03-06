01e12bb8b0411e28bbb2883cb941e78828df59f8
Patenaude, Christopher T <patenauc@oregonstate.edu>
2020-03-06 15:05:26 -0800

    Setup testing framework and test Imu

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-# Project root Directory
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+IMU_PATH = "TODO"
+IMU_MOCK_PATH = os.path.join(ROOT_DIR, 'test/mocks/IMU_timestamped_test_data.bin')
