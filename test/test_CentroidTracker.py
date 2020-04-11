ed16e13e86c1d5834a5a85b7c947106f8936166a
(Donald) Max Harkins <harkindo@oregonstate.edu>
2020-04-11 14:09:27 -0700

    Added centroid tracker test

---
+++ test/test_CentroidTracker.py
@@ -0,0 +1,34 @@
+import unittest
+from object_detection.CentroidTracker import CentroidTracker
+
+
+class Test_CentroidTracker(unittest.TestCase):
+
+    #@classmethod
+    # def setUpClass(cls):
+    #    cls.tracker = CentroidTracker()
+
+    def test_register_deregister(self):
+        tracker = CentroidTracker()
+        print(tracker.nextObjectID)
+        self.assertEqual(len(tracker.objects), tracker.nextObjectID)
+
+        testCentroid = (0,0)
+        testData = None
+        numObjects = 10
+
+        # Register 10 objects
+        for i in range(1, numObjects+1):
+            tracker.register(testCentroid, testData)
+            self.assertEqual(tracker.nextObjectID, i)
+            self.assertEqual(len(tracker.objects), i)
+            self.assertEqual(len(tracker.objects), len(tracker.objectData))
+
+        # Deregister Objects
+        for i in range(0, numObjects):
+            tracker.deregister(i)
+            self.assertEqual(len(tracker.objects), len(tracker.objectData))
+            self.assertEqual(len(tracker.objects), numObjects - (i+1))
+            self.assertEqual(tracker.nextObjectID, numObjects)
+
+        self.assertFalse(any(tracker.objects))
